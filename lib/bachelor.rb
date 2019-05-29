0e9a7660e1458d77da1be75253417dec6c35910c
Zach Parker <parkerz410@gmail.com>
2019-05-29 14:24:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,21 +1,16 @@
 require "pry"
 
 def get_first_name_of_season_winner(data, season)
-  name_array = []
-  name_string =""
   data.each do |season_num, details_1|
     # binding.pry
-    season_num.to_sym.each do |details_2|
-      binding.pry
+    # season_num.to_sym.each do |details_2|
+      # binding.pry
       # if season_num == season && details_1[:status] == "Winner"
       # binding.pry
-        name_array << details_1[:name].split(" ")
-        name_string = name_array[0]
+        details_1[:name].split(" ")[0]
       # end
-    end
+    # end
   end
-  # binding.pry
-  return name_string
 end
 
 def get_contestant_name(data, occupation)
