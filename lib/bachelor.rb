845773bb2ac4678ce5d7f86fbc85c917f0380ace
Zach Parker <parkerz410@gmail.com>
2019-05-29 15:00:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,7 +8,7 @@ def get_first_name_of_season_winner(data, season)
       # binding.pry
       if info == "Winner"
          first_name = individuals["name"]
-         first_name.split(" ").first
+         first_name.split(" ").[0]
       end
     end
   end
