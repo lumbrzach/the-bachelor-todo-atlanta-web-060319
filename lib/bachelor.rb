cbbc40123811dc2fa16f155a1769627405082bac
Zach Parker <parkerz410@gmail.com>
2019-05-29 15:04:03 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,15 @@
 require "pry"
 
 def get_first_name_of_season_winner(data, season)
-  first_name = ""
   data[season].each do |individuals|
     # binding.pry
     individuals.each do |attributes, info|
       binding.pry
       if info == "Winner"
-         first_name = individuals["name"].split(" ")[0]
+        individuals["name"].split(" ")[0]
       end
     end
   end
-  return first_name
 end
 
 def get_contestant_name(data, occupation)
