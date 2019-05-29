4e3079dc7d31d9e1f17c3157a8b8195ff16892a2
Zach Parker <parkerz410@gmail.com>
2019-05-29 15:09:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,7 +13,13 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, individuals|
+    individuals.each do |attributes, info|
+      if "occupation" == occupation
+        return individuals["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
