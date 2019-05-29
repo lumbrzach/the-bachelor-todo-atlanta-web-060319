64196dd57cc70a3d34ee7c1ed30a9bc8fc5a113e
Zach Parker <parkerz410@gmail.com>
2019-05-29 16:10:20 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -46,7 +46,7 @@ def get_occupation(data, hometown)
     individuals.each do |attributes|
       if attributes["hometown"] == hometown
         binding.pry
-        contestant = attributes["occupation"].find
+        contestant = attributes.find("occupation")
       end
     end
   end
