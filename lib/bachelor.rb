31bcd7fab465c856ad56adebb264327ef969fbb8
Zach Parker <parkerz410@gmail.com>
2019-05-29 14:44:30 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def get_first_name_of_season_winner(data, season)
   data.each do |season_num, attrib|
-    binding.pry
+    # binding.pry
     if season == season_num
       attrib.each do |attributes|
         if attributes[:status] == "Winner"
